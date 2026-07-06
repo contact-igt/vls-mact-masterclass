72b979d7cd663d92698803fea69ae11d03f01c12
Veeravel-S <veeravel.igt@gmail.com>
2026-07-06 12:27:20 +0530

    changes

--- src/constants/Home/index.js
+++ src/constants/Home/index.js
@@ -11,7 +11,7 @@ export const courseDetails = {
 
 export const mactPayment = {
   title: 'Motor Accident Claims Practice — 3 Hours Masterclass',
-  amount: 1,
+  amount: 499,
   currency: 'INR',
   programmDate: '2026-07-12',
   pageName: 'mact-masterclass',
